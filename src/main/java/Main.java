25b86d0e45821a848a5c02ca190929383711995f
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-24 19:36:03 +0200

    Added line 12 in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("Mitt namn är Magnus");
         System.out.println("Dagens datum är 2026-08-24");
         System.out.println("Tack och hej!");
+        System.out.println("Mvh. En blivande Javautvecklare.");
     }
 }
