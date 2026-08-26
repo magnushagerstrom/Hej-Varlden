c6fd07acd9acedfb6f4f53cf7ecadd4a26579190
MagnusHagerstrom <magnus.hagerstrom@gmail.com>
2026-08-26 09:22:18 +0200

    Added lines 11-14

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
@@ -10,5 +8,9 @@ public class Main {
         System.out.println("Dagens datum är 2026-08-24");
         System.out.println("Tack och hej!");
         System.out.println("Mvh. En blivande Javautvecklare.");
+        System.out.println("------Lektion-övning------");
+        System.out.println("Hej världen!");
+        System.out.println("Mitt namn är Magnus");
+        System.out.println("Dagens datum är 2026-08-26");
     }
 }
